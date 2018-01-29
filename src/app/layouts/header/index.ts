b6ae166779658b4d8c9f687b719645a0c2e5500c
Deberson de Paula <deberson.paula@gmail.com>
2018-01-29 19:37:04 -0200

    corrections

--- src/app/layouts/header/index.ts
+++ src/app/layouts/header/index.ts
@@ -1,4 +1,6 @@
-import { Component, ViewEncapsulation, Output, EventEmitter } from '@angular/core';
+import { Component, ViewEncapsulation, Output, EventEmitter, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { RouterEvent } from '@angular/router/src/events';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +8,18 @@ import { Component, ViewEncapsulation, Output, EventEmitter } from '@angular/cor
   styleUrls: ['./header.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class Header {
+export class Header implements OnInit {
   @Output() toggleSidenav = new EventEmitter<void>();
+
+  constructor(private _router: Router) {
+    this._router.events.subscribe((value: RouterEvent) => {
+      // value
+    });
+  }
+
+  ngOnInit() {
+    //this._router.events.subscribe(() => {
+      //console.log(this._route.snapshot.data['caption']);
+    //});
+  }
 }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,6 +3,7 @@ import { HomePage } from './views/home';
 import { TestComponent } from './views/test/test';
 
 export const AppRoutes: Routes = [
-    {path: '', component: HomePage},
-    {path: 'test', component: TestComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: HomePage, data: {caption: 'Home'}},
+    {path: 'test', component: TestComponent, data: {caption: 'Test'}},
 ];
